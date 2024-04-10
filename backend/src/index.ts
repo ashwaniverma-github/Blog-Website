ed43ad485b481636258f775c9e13cdc79d288b57
ashwaniverma-github <ap8606574@gmail.com>
2024-04-10 22:40:02 +0530

    initialized beckend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,33 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signup' ,(c)=>{
+  return c.text("Signup route")
 })
+
+app.post('api/v1/signin',(c)=>{
+  return c.text('Signin route')
+})
+
+app.post('api/v1/blog',(c)=>{
+  return c.text("Blog")
+})
+
+app.put('api/v1/blog',(c)=>{
+  return c.text("BLog")
+})
+
+app.get('api/v1/blog/:id',(c)=>{
+  const id = c.req.param('id')
+  console.log(id)
+  return c.text("get blog route")
+})
+
+app.get('/api/v1/blog/bulk',(c)=>{
+  return c.text("get blog")
+})
+
+
+
 
 export default app
