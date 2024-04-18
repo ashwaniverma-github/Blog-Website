04cb0fd1e800896d27c7c51c2bca9b99ba8ede14
ashwaniverma-github <ap8606574@gmail.com>
2024-04-18 09:24:22 +0530

    Initialized Frontend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import {Signup} from './pages/Signup'
+import { Signin } from './pages/Signin'
+import { Blog } from './pages/Blog'
+
+function App(){
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element= {<Signup/>} />
+          <Route path='/signin' element = {<Signin/>}/>
+          <Route path='/blog/:id' element = {<Blog/>}/>
+        </Routes>
+      </BrowserRouter>
+      </>
+  )
+
+}
+
+export default App
